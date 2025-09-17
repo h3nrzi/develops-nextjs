f19ef3126cae2f0b74eef30b93864554a771bb66
h3nrzi <rezaeig22@gmail.com>
2025-09-17 22:19:04 +0330

    chore: Enhance Tailwind CSS setup with new utilities, update theme colors, and add typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,72 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        primary: '#3b82f6',
-        secondary: '#64748b',
-      },
-      spacing: {
-        '18': '4.5rem',
+        primary: {
+          500: "#FF7000",
+          100: "#FFF1E6",
+        },
+        dark: {
+          500: "#101012",
+          400: "#212734",
+          300: "#151821",
+          200: "#0F1117",
+          100: "#000000",
+        },
+        light: {
+          900: "#FFFFFF",
+          800: "#F4F6F8",
+          850: "#FDFDFD",
+          700: "#DCE3F1",
+          500: "#7B8EC8",
+          400: "#858EAD",
+        },
+        "accent-blue": "#1DA1F2",
+        fontFamily: {
+          inter: ["var(--font-inter)"],
+          spaceGrotesk: ["var(--font-spaceGrotesk)"],
+        },
+        boxShadow: {
+          "light-100":
+            "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px",
+          "light-200": "10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+          "light-300": "-10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+          "dark-100": "0px 2px 10px 0px rgba(46, 52, 56, 0.10)",
+          "dark-200": "2px 0px 20px 0px rgba(39, 36, 36, 0.04)",
+        },
+        backgroundImage: {
+          "auth-dark": "url('/assets/images/auth-dark.png')",
+          "auth-light": "url('/assets/images/auth-light.png')",
+        },
+        screens: {
+          xs: "420px",
+        },
+        keyframes: {
+          "accordion-down": {
+            from: { height: 0 },
+            to: { height: "var(--radix-accordion-content-height)" },
+          },
+          "accordion-up": {
+            from: { height: "var(--radix-accordion-content-height)" },
+            to: { height: 0 },
+          },
+        },
+        animation: {
+          "accordion-down": "accordion-down 0.2s ease-out",
+          "accordion-up": "accordion-up 0.2s ease-out",
+        },
+        plugins: [
+          import("tailwindcss-animate"),
+          import("@tailwindcss/typography"),
+        ],
       },
     },
   },
   plugins: [],
-}
+};
