d3f51bedfa60e08eba3ea6949283b86dae042737
h3nrzi <rezaeig22@gmail.com>
2025-09-23 10:57:42 +0330

    feat: Add layout component and navbar, update home page structure, and enhance Tailwind CSS configuration

--- src/app/(root)/(home)/page.tsx
+++ src/app/(root)/(home)/page.tsx
@@ -1,10 +1,3 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello Js Mastery</h1>
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div></div>;
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,45 +28,41 @@ module.exports = {
           400: "#858EAD",
         },
         "accent-blue": "#1DA1F2",
-        fontFamily: {
-          inter: ["var(--font-inter)"],
-          spaceGrotesk: ["var(--font-spaceGrotesk)"],
+      },
+      fontFamily: {
+        inter: ["var(--font-inter)"],
+        spaceGrotesk: ["var(--font-spaceGrotesk)"],
+      },
+      boxShadow: {
+        "light-100":
+          "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px",
+        "light-200": "10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "light-300": "-10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
+        "dark-100": "0px 2px 10px 0px rgba(46, 52, 56, 0.10)",
+        "dark-200": "2px 0px 20px 0px rgba(39, 36, 36, 0.04)",
+      },
+      backgroundImage: {
+        "auth-dark": "url('/assets/images/auth-dark.png')",
+        "auth-light": "url('/assets/images/auth-light.png')",
+      },
+      screens: {
+        xs: "420px",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
         },
-        boxShadow: {
-          "light-100":
-            "0px 12px 20px 0px rgba(184, 184, 184, 0.03), 0px 6px 12px 0px",
-          "light-200": "10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
-          "light-300": "-10px 10px 20px 0px rgba(218, 213, 213, 0.10)",
-          "dark-100": "0px 2px 10px 0px rgba(46, 52, 56, 0.10)",
-          "dark-200": "2px 0px 20px 0px rgba(39, 36, 36, 0.04)",
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
         },
-        backgroundImage: {
-          "auth-dark": "url('/assets/images/auth-dark.png')",
-          "auth-light": "url('/assets/images/auth-light.png')",
-        },
-        screens: {
-          xs: "420px",
-        },
-        keyframes: {
-          "accordion-down": {
-            from: { height: 0 },
-            to: { height: "var(--radix-accordion-content-height)" },
-          },
-          "accordion-up": {
-            from: { height: "var(--radix-accordion-content-height)" },
-            to: { height: 0 },
-          },
-        },
-        animation: {
-          "accordion-down": "accordion-down 0.2s ease-out",
-          "accordion-up": "accordion-up 0.2s ease-out",
-        },
-        plugins: [
-          import("tailwindcss-animate"),
-          import("@tailwindcss/typography"),
-        ],
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
-  plugins: [],
+  plugins: [import("tailwindcss-animate"), import("@tailwindcss/typography")],
 };
