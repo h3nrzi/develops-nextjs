35131172c88d3bccd1a183ebe39bc0edab1d6337
h3nrzi <rezaeig22@gmail.com>
2025-09-25 23:11:48 +0330

    feat: Add mobile navigation component, global search input, and integrate Radix UI dialog for improved user experience

--- src/components/shared/navbar/navbar.tsx
+++ src/components/shared/navbar/navbar.tsx
@@ -2,12 +2,15 @@ import { SignedIn, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 
+import GlobalSearch from "../search/global-search";
+
+import MobileNav from "./mobile-nav";
 import Theme from "./theme";
 
 function Navbar() {
   return (
-    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 sm:px-12 dark:shadow-none">
-      {/* Logo */}
+    <nav className="flex-between background-light900_dark200 fixed z-50 w-full gap-5 p-6 shadow-light-300 dark:shadow-none sm:px-12">
+      {/* */}
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -15,13 +18,13 @@ function Navbar() {
           height={23}
           alt={"DevFlow"}
         />
-        <p className="h2-bold font-spaceGrotesk text-dark-100 max-sm:hidden dark:text-light-900">
+        <p className="h2-bold font-spaceGrotesk text-dark-100 dark:text-light-900 max-sm:hidden">
           Dev <span className="text-primary-500">OverFlow</span>
         </p>
       </Link>
-      {/* GlobalSearch */}
-      GlobalSearch
-      {/* Desktop Nav */}
+      {/* */}
+      <GlobalSearch />
+      {/* */}
       <div className="flex-between gap-5">
         <Theme />
         <SignedIn>
@@ -34,7 +37,8 @@ function Navbar() {
           />
         </SignedIn>
       </div>
-      {/* Mobile Nav */}
+      {/* */}
+      <MobileNav />
     </nav>
   );
 }
