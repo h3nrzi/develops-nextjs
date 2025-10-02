2328f658fa0990f97bfbb45266b2eb9710082435
h3nrzi <rezaeig22@gmail.com>
2025-10-02 11:09:53 +0330

    feat: Update Home page layout to include a question button and search/filter sections for improved user interaction

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,3 +1,23 @@
+import Link from "next/link";
+
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Home Page</div>;
+  return (
+    <>
+      <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">تمام سوالات</h1>
+        <Link href="/ask-question" className="flex justify-end max-sm:w-full">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
+            سوال بپرسید
+          </Button>
+        </Link>
+      </div>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <div>LocalSearchbar</div>
+        <div>Filters</div>
+      </div>
+    </>
+  );
 }
